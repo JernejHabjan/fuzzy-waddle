efadd2e7df9f9c93cd5655a50f2ea69901b260d7
Jernej Habjan <jhabjan@marinexchange.com>
2022-10-10 11:48:09 +0200

    Adding properties to tilesets

--- apps/client/src/assets/probable-waffle/tilesets/iso-64x64-building.tsx
+++ apps/client/src/assets/probable-waffle/tilesets/iso-64x64-building.tsx
@@ -1,4 +1,56 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="iso-64x64-building" tilewidth="64" tileheight="64" tilecount="80" columns="4">
  <image source="../atlas/iso-64x64-building.png" trans="000000" width="256" height="1280"/>
+ <tile id="0">
+  <properties>
+   <property name="step-height" type="int" value="32"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="step-height" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="step-height" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="step-height" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="step-height" type="int" value="32"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="step-height" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="slope-direction" value="south-east"/>
+   <property name="step-height" type="int" value="16"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="slope-direction" value="south-west"/>
+   <property name="step-height" type="int" value="16"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="step-height" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="step-height" type="int" value="0"/>
+  </properties>
+ </tile>
 </tileset>
