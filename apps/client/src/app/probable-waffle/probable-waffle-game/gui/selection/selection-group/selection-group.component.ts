94c08b6ec9f5f7f04aa9ee6351ab61795bb510bf
Jernej Habjan <jhabjan@marinexchange.com>
2022-10-23 19:36:24 +0200

    Fixed tests

--- apps/client/src/app/probable-waffle/probable-waffle-game/gui/selection/selection-group/selection-group.component.ts
+++ apps/client/src/app/probable-waffle/probable-waffle-game/gui/selection/selection-group/selection-group.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fuzzy-waddle-selection-group',
   templateUrl: './selection-group.component.html',
   styleUrls: ['./selection-group.component.scss']
 })
-export class SelectionGroupComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class SelectionGroupComponent {}
