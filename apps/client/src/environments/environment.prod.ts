7c32aa5ea8a9013f54ed6e427a8295801c028caa
Jernej Habjan <jhabjan@marinexchange.com>
2023-01-15 15:51:10 +0100

    API url via environmental variable

--- apps/client/src/environments/environment.prod.ts
+++ apps/client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment-type';
 
 export const environment: Environment = {
   production: true,
-  api: process.env['API_URL'] || 'https://fuzzy-waddle-api-production.up.railway.app/'
+  api: process.env['API_URL'] || 'http://setup-via-env-variable/'
 };
