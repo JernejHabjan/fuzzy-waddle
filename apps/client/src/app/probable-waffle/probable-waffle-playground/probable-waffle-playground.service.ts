eb1b95a62e1f54378018b9322fc1cad1285914bf
Jernej Habjan <jhabjan@marinexchange.com>
2023-01-15 10:41:03 +0100

    Environment and removal of deprecated files

--- apps/client/src/app/probable-waffle/probable-waffle-playground/probable-waffle-playground.service.ts
+++ apps/client/src/app/probable-waffle/probable-waffle-playground/probable-waffle-playground.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Message } from '@fuzzy-waddle/api-interfaces';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class ProbableWafflePlaygroundService {
   constructor(private http: HttpClient) {}
 
   getData() {
-    return this.http.get<Message>('/api/hello');
+    return this.http.get<Message>(environment.api + 'api/hello');
   }
 }
--- apps/client/src/environments/environment.prod.ts
+++ apps/client/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true,
+import { Environment } from './environment-type';
+
+export const environment: Environment = {
+  production: false,
+  api: 'http://localhost:3333/'
 };
