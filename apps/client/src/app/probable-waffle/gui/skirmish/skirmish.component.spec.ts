f215ccf33f92868ca16cb295b5976ebc1c07c468
Jernej Habjan <jernej.habjan96@gmail.com>
2023-01-22 13:53:53 +0100

    PNPM (#48)
    * pnpm added
    
    * Removing GH templates
    
    * Github actions fixes
    
    * npm update
    
    * Test runner fix
    
    * More modules
    
    * Fix tests
    
    * Jest fail on console fixes
    
    * test in package.json
    
    Co-authored-by: Jernej Habjan <jhabjan@marinexchange.com>

--- apps/client/src/app/probable-waffle/gui/skirmish/skirmish.component.spec.ts
+++ apps/client/src/app/probable-waffle/gui/skirmish/skirmish.component.spec.ts
@@ -2,6 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SkirmishComponent } from './skirmish.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { PlayerDefinitionComponent } from './player-definition/player-definition.component';
+import { MapSelectorComponent } from './map-selector/map-selector.component';
+import { MapDefinitionComponent } from './map-definition/map-definition.component';
+import { GameModeDefinitionComponent } from './game-mode-definition/game-mode-definition.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
 
 describe('SkirmishComponent', () => {
   let component: SkirmishComponent;
@@ -9,8 +15,14 @@ describe('SkirmishComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SkirmishComponent],
-      imports: [RouterTestingModule]
+      declarations: [
+        SkirmishComponent,
+        PlayerDefinitionComponent,
+        MapSelectorComponent,
+        MapDefinitionComponent,
+        GameModeDefinitionComponent
+      ],
+      imports: [RouterTestingModule, FormsModule, FontAwesomeTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SkirmishComponent);
