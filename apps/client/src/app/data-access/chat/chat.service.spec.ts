e0eab4179ad94ccf73bda763ef2c4e17bcfec3fd
Jernej Habjan <jhabjan@marinexchange.com>
2023-03-19 12:21:00 +0100

    Try socket-io fix 1

--- apps/client/src/app/data-access/chat/chat.service.spec.ts
+++ apps/client/src/app/data-access/chat/chat.service.spec.ts
@@ -1,14 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ChatService } from './chat.service';
-import { SocketIoModule } from 'ngx-socket-io';
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import { environment } from '../../../environments/environment';
+
+const config: SocketIoConfig = { url: 'http://localhost:3005', options: {} };
 
 describe('Chat', () => {
   let service: ChatService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [SocketIoModule.forRoot(environment.socketIoConfig)] });
+    TestBed.configureTestingModule({ imports: [SocketIoModule.forRoot(config)] });
     service = TestBed.inject(ChatService);
   });
 
