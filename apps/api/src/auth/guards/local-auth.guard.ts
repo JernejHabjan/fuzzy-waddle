694dca1ecc564befa685594c77129a330a7a4926
Jernej Habjan <jhabjan@marinexchange.com>
2023-03-19 12:50:13 +0100

    Adding comment for socket-io fix

--- apps/api/src/auth/guards/local-auth.guard.ts
+++ apps/api/src/auth/guards/local-auth.guard.ts
@@ -1,6 +1,2 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthStrategies } from '../strategies/auth-strategies';
-
-@Injectable()
-export class LocalAuthGuard extends AuthGuard(AuthStrategies.local) {}
+// @Injectable()
+// export class LocalAuthGuard extends AuthGuard(AuthStrategies.local) {}
