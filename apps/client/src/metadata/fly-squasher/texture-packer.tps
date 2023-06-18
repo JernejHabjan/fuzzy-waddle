df717331a8e700387388428c2ebde78c19b30d46
Jernej Habjan <jhabjan@marinexchange.com>
2023-06-18 10:47:48 +0200

    Phaser editor project

--- apps/client/src/metadata/fly-squasher/texture-packer.tps
+++ apps/client/src/metadata/fly-squasher/texture-packer.tps
@@ -64,7 +64,7 @@
             </struct>
         </struct>
         <key>shapePadding</key>
-        <uint>0</uint>
+        <uint>1</uint>
         <key>jpgQuality</key>
         <uint>80</uint>
         <key>pngOptimizationLevel</key>
@@ -245,6 +245,21 @@
                 <rect>11,5,23,10</rect>
                 <key>scale9Paddings</key>
                 <rect>11,5,23,10</rect>
+                <key>scale9FromFile</key>
+                <false/>
+            </struct>
+            <key type="filename">fly/suckler.png</key>
+            <struct type="IndividualSpriteSettings">
+                <key>pivotPoint</key>
+                <point_f>0.5,0.5</point_f>
+                <key>spriteScale</key>
+                <double>1</double>
+                <key>scale9Enabled</key>
+                <false/>
+                <key>scale9Borders</key>
+                <rect>2,2,3,5</rect>
+                <key>scale9Paddings</key>
+                <rect>2,2,3,5</rect>
                 <key>scale9FromFile</key>
                 <false/>
             </struct>
---
+++ apps/client/src/metadata/phaser-editor/src/prefabs/Fly.ts
@@ -0,0 +1,91 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Fly extends Phaser.GameObjects.Container {
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number) {
+		super(scene, x ?? 0, y ?? 0);
+
+		// leg_back_right
+		const leg_back_right = scene.add.image(14.450684318372382, -0.6341442773239621, "fly-squasher-spritesheet", "fly/leg-back");
+		leg_back_right.setOrigin(0, 1);
+		this.add(leg_back_right);
+
+		// leg_back_left
+		const leg_back_left = scene.add.image(-15.549315681627625, -2.634144277323962, "fly-squasher-spritesheet", "fly/leg-back");
+		leg_back_left.setOrigin(1, 1);
+		leg_back_left.flipX = true;
+		this.add(leg_back_left);
+
+		// leg_front_right
+		const leg_front_right = scene.add.image(12.450684318372382, 31.365855722676038, "fly-squasher-spritesheet", "fly/leg-front");
+		leg_front_right.setOrigin(0, 0);
+		this.add(leg_front_right);
+
+		// leg_front_left
+		const leg_front_left = scene.add.image(-13.549315681627625, 31.365855722676038, "fly-squasher-spritesheet", "fly/leg-front");
+		leg_front_left.setOrigin(1, 0);
+		leg_front_left.flipX = true;
+		this.add(leg_front_left);
+
+		// leg_middle_right
+		const leg_middle_right = scene.add.image(12.450684318372382, 8.365855722676038, "fly-squasher-spritesheet", "fly/leg-middle");
+		leg_middle_right.setOrigin(0, 0.5);
+		this.add(leg_middle_right);
+
+		// leg_middle_left
+		const leg_middle_left = scene.add.image(-13.549315681627625, 7.365855722676038, "fly-squasher-spritesheet", "fly/leg-middle");
+		leg_middle_left.setOrigin(1, 0.5);
+		leg_middle_left.flipX = true;
+		this.add(leg_middle_left);
+
+		// body
+		const body = scene.add.image(0, -4, "fly-squasher-spritesheet", "fly/body");
+		this.add(body);
+
+		// wing_left
+		const wing_left = scene.add.image(-16, 21, "fly-squasher-spritesheet", "fly/wing");
+		wing_left.setOrigin(0.27, 0.98);
+		this.add(wing_left);
+
+		// wing_right
+		const wing_right = scene.add.image(15.770684318372375, 22.39585572267604, "fly-squasher-spritesheet", "fly/wing");
+		wing_right.setOrigin(0.7, 0.99);
+		wing_right.flipX = true;
+		this.add(wing_right);
+
+		// head_container
+		const head_container = scene.add.container(-0.949315681627624, 35.59585572267604);
+		this.add(head_container);
+
+		// fly_head
+		const fly_head = scene.add.image(0.4, 0, "fly-squasher-spritesheet", "fly/head");
+		fly_head.setOrigin(0.5, 0);
+		head_container.add(fly_head);
+
+		// fly_suckler
+		const fly_suckler = scene.add.image(0, 19.8, "fly-squasher-spritesheet", "fly/suckler");
+		fly_suckler.setOrigin(0.58, 0);
+		head_container.add(fly_suckler);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
