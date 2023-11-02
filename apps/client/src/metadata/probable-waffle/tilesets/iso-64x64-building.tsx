fdd937d1c0229bd397245b0402fac7cf0ef5905d
Jernej Habjan <jhabjan@marinexchange.com>
2023-11-02 13:21:15 +0100

    Isometric tilesets

--- apps/client/src/metadata/probable-waffle/tilesets/iso-64x64-building.tsx
+++ apps/client/src/metadata/probable-waffle/tilesets/iso-64x64-building.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="iso-64x64-building" tilewidth="64" tileheight="64" tilecount="80" columns="4">
+ <grid orientation="isometric" width="64" height="64"/>
  <image source="../../../assets/probable-waffle/atlas/iso-64x64-building.png" trans="000000" width="256" height="1280"/>
  <tile id="0">
   <properties>
