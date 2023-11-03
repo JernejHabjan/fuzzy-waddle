533314d2edab8a9f909c5dcd3cb3a1b7af516ea2
Jernej Habjan <jhabjan@marinexchange.com>
2023-11-03 11:48:00 +0100

    Sample tiled view

---
+++ apps/client/src/metadata/probable-waffle/tilesets/samples/64_64.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="64_64" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="5">
+  <image width="64" height="64" source="../../sprites/characters/general/warrior.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../sprites/characters/tivara/worker_male.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../sprites/characters/tivara/archer_female.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../sprites/characters/tivara/maceman_male.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../sprites/characters/tivara/worker_female.png"/>
+ </tile>
+</tileset>
