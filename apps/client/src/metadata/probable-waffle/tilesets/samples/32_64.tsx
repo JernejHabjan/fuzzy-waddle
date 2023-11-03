538798d3db5a2e8920424f0f7a168e6038a3ddd3
Jernej Habjan <jhabjan@marinexchange.com>
2023-11-03 10:00:26 +0100

    Tiled SampleDisplay layer

---
+++ apps/client/src/metadata/probable-waffle/tilesets/samples/32_64.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="32_64" tilewidth="32" tileheight="64" tilecount="1" columns="0">
+ <grid orientation="isometric" width="32" height="64"/>
+ <tile id="0">
+  <image width="32" height="64" source="../../sprites/buildings/tivara/olival.png"/>
+ </tile>
+</tileset>
