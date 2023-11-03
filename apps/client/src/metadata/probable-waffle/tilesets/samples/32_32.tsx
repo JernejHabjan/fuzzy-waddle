ea3bc3948aaf1601275a9866034599df10ca1086
Jernej Habjan <jhabjan@marinexchange.com>
2023-11-03 18:39:02 +0100

    Rolling olival

---
+++ apps/client/src/metadata/probable-waffle/tilesets/samples/32_32.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="32_32" tilewidth="32" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="32" height="32" source="../../sprites/buildings/tivara/olival_small/olival_small-2.png"/>
+  <animation>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../sprites/buildings/tivara/olival_small/olival_small-0.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../sprites/buildings/tivara/olival_small/olival_small-1.png"/>
+ </tile>
+</tileset>
