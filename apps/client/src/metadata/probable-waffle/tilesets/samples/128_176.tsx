f4820d5bf05b80ca860361f19b0985f470fa9bd5
Jernej Habjan <jhabjan@marinexchange.com>
2023-11-04 19:56:01 +0100

    Wall 2 tiles each

---
+++ apps/client/src/metadata/probable-waffle/tilesets/samples/128_176.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="128_176" tilewidth="128" tileheight="176" tilecount="1" columns="0">
+ <tileoffset x="0" y="16"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="176" source="../../sprites/buildings/tivara/watchtower.png"/>
+ </tile>
+</tileset>
---
+++ apps/client/src/metadata/probable-waffle/tilesets/samples/96_112.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="96_112" tilewidth="96" tileheight="112" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall02.png"/>
+ </tile>
+ <tile id="2">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall03.png"/>
+ </tile>
+ <tile id="3">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall04.png"/>
+ </tile>
+ <tile id="4">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall05.png"/>
+ </tile>
+ <tile id="5">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall06.png"/>
+ </tile>
+ <tile id="6">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall07.png"/>
+ </tile>
+ <tile id="7">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall08.png"/>
+ </tile>
+ <tile id="8">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall09.png"/>
+ </tile>
+ <tile id="9">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="96" height="112" source="../../sprites/buildings/tivara/wall/wall00.png"/>
+ </tile>
+</tileset>
