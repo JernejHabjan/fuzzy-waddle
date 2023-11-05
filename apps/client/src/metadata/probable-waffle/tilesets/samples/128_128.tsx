cc748173a55396682a9b6629ce23fcc6c0de199d
Jernej Habjan <jhabjan@marinexchange.com>
2023-11-05 12:43:51 +0100

    Skaduwee

--- apps/client/src/metadata/probable-waffle/tilesets/samples/128_128.tsx
+++ apps/client/src/metadata/probable-waffle/tilesets/samples/128_128.tsx
@@ -1,7 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="128_128" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="128_128" tilewidth="128" tileheight="192" tilecount="8" columns="0">
  <grid orientation="isometric" width="128" height="128"/>
  <tile id="0">
   <image width="128" height="128" source="../../sprites/buildings/tivara/workmill.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../sprites/buildings/skaduwee/barracks/barracks.png"/>
+ </tile>
+ <tile id="4">
+  <image width="27" height="61" source="../../sprites/buildings/skaduwee/barracks/barracks-entrance/barracks-entrance-1.png"/>
+  <animation>
+   <frame tileid="4" duration="1000"/>
+   <frame tileid="5" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <image width="27" height="61" source="../../sprites/buildings/skaduwee/barracks/barracks-entrance/barracks-entrance-0.png"/>
+ </tile>
+ <tile id="6">
+  <image width="12" height="24" source="../../sprites/buildings/skaduwee/barracks/cloud-vertical.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="192" source="../../sprites/buildings/skaduwee/owlery/owlery.png"/>
+ </tile>
+ <tile id="8">
+  <image width="22" height="22" source="../../sprites/buildings/skaduwee/owlery/owlery-owl/owlery-owl-1.png"/>
+  <animation>
+   <frame tileid="9" duration="5000"/>
+   <frame tileid="8" duration="1000"/>
+   <frame tileid="9" duration="5000"/>
+   <frame tileid="9" duration="5000"/>
+   <frame tileid="8" duration="2000"/>
+   <frame tileid="9" duration="5000"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <image width="22" height="22" source="../../sprites/buildings/skaduwee/owlery/owlery-owl/owlery-owl-0.png"/>
  </tile>
 </tileset>
