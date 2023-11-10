c0f91283759f40d2dfd1ce1b5449dbf00195cdfe
Jernej Habjan <jhabjan@marinexchange.com>
2023-11-10 17:36:48 +0100

    Disable mouse move

--- apps/client/src/metadata/phaser-editor/src/ProbableWaffle/scenes/Map1.ts
+++ apps/client/src/metadata/phaser-editor/src/ProbableWaffle/scenes/Map1.ts
@@ -2,70 +2,68 @@
 
 /* START OF COMPILED CODE */
 
-import Phaser from "phaser";
-import Sandhold from "../prefabs/Sandhold";
-import Owlery from "../prefabs/Owlery";
-import Bridge from "../prefabs/Bridge";
+import Phaser from 'phaser';
+import Sandhold from '../prefabs/Sandhold';
+import Owlery from '../prefabs/Owlery';
+import Bridge from '../prefabs/Bridge';
 /* START-USER-IMPORTS */
 /* END-USER-IMPORTS */
 
 export default class Map1 extends Phaser.Scene {
+  constructor() {
+    super('Map1');
 
-	constructor() {
-		super("Map1");
-
-		/* START-USER-CTR-CODE */
+    /* START-USER-CTR-CODE */
     // Write your code here.
     /* END-USER-CTR-CODE */
-	}
+  }
 
-	editorCreate(): void {
+  editorCreate(): void {
+    // tiles
+    const tiles = this.add.tilemap('tiles');
+    tiles.addTilesetImage('tiles', 'tiles_1');
 
-		// tiles
-		const tiles = this.add.tilemap("tiles");
-		tiles.addTilesetImage("tiles", "tiles_1");
+    // tiles_1
+    const tiles_1 = this.add.tilemap('tiles');
+    tiles_1.addTilesetImage('tiles', 'tiles_1');
 
-		// tiles_1
-		const tiles_1 = this.add.tilemap("tiles");
-		tiles_1.addTilesetImage("tiles", "tiles_1");
+    // tileMap_level
+    tiles_1.createLayer('TileMap_level_1', ['tiles'], 608, -272);
 
-		// tileMap_level
-		tiles_1.createLayer("TileMap_level_1", ["tiles"], 608, -272);
+    // sandhold
+    const sandhold = new Sandhold(this, 864, 368);
+    this.add.existing(sandhold);
 
-		// sandhold
-		const sandhold = new Sandhold(this, 864, 368);
-		this.add.existing(sandhold);
+    // owlery
+    const owlery = new Owlery(this, 703, 286);
+    this.add.existing(owlery);
 
-		// owlery
-		const owlery = new Owlery(this, 703, 286);
-		this.add.existing(owlery);
+    // bridge
+    const bridge = new Bridge(this, 544, 592);
+    this.add.existing(bridge);
 
-		// bridge
-		const bridge = new Bridge(this, 544, 592);
-		this.add.existing(bridge);
+    // idle_downwarrior_idle
+    const idle_downwarrior_idle = this.add.sprite(800, 368, 'warrior_idle', 4);
+    idle_downwarrior_idle.play('idle_downwarrior_idle');
 
-		// idle_downwarrior_idle
-		const idle_downwarrior_idle = this.add.sprite(800, 368, "warrior_idle", 4);
-		idle_downwarrior_idle.play("idle_downwarrior_idle");
+    // large_thrust_right
+    const large_thrust_right = this.add.sprite(896, 368, 'magician_female_large_thrust', 21);
+    large_thrust_right.play('large_thrust_right');
 
-		// large_thrust_right
-		const large_thrust_right = this.add.sprite(896, 368, "magician_female_large_thrust", 21);
-		large_thrust_right.play("large_thrust_right");
+    // shoot_left
+    const shoot_left = this.add.sprite(1024, 368, 'ranged_female_shoot', 12);
+    shoot_left.play('shoot_left');
 
-		// shoot_left
-		const shoot_left = this.add.sprite(1024, 368, "ranged_female_shoot", 12);
-		shoot_left.play("shoot_left");
+    this.tiles = tiles;
+    this.tiles_1 = tiles_1;
 
-		this.tiles = tiles;
-		this.tiles_1 = tiles_1;
+    this.events.emit('scene-awake');
+  }
 
-		this.events.emit("scene-awake");
-	}
+  private tiles!: Phaser.Tilemaps.Tilemap;
+  private tiles_1!: Phaser.Tilemaps.Tilemap;
 
-	private tiles!: Phaser.Tilemaps.Tilemap;
-	private tiles_1!: Phaser.Tilemaps.Tilemap;
-
-	/* START-USER-CODE */
+  /* START-USER-CODE */
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private controlConfig!: Phaser.Types.Cameras.Controls.FixedKeyControlConfig;
   private controls!: Phaser.Cameras.Controls.FixedKeyControl;
@@ -97,17 +95,17 @@ export default class Map1 extends Phaser.Scene {
     const cam = this.cameras.main;
     const pointer = this.input.activePointer;
 
-    if (pointer.x < this.edgeThickness) {
-      cam.scrollX -= this.moveSpeed;
-    } else if (pointer.x > cam.width - this.edgeThickness) {
-      cam.scrollX += this.moveSpeed;
-    }
-
-    if (pointer.y < this.edgeThickness) {
-      cam.scrollY -= this.moveSpeed;
-    } else if (pointer.y > cam.height - this.edgeThickness) {
-      cam.scrollY += this.moveSpeed;
-    }
+    // if (pointer.x < this.edgeThickness) {
+    //   cam.scrollX -= this.moveSpeed;
+    // } else if (pointer.x > cam.width - this.edgeThickness) {
+    //   cam.scrollX += this.moveSpeed;
+    // }
+    //
+    // if (pointer.y < this.edgeThickness) {
+    //   cam.scrollY -= this.moveSpeed;
+    // } else if (pointer.y > cam.height - this.edgeThickness) {
+    //   cam.scrollY += this.moveSpeed;
+    // }
   }
 
   /* END-USER-CODE */
