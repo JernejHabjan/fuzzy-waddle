5a4e2007be5d8bc63395416d9c7703855b29a2a5
Jernej Habjan <jhabjan@marinexchange.com>
2023-12-23 20:19:12 +0100

    Tiles 2

--- apps/client/src/metadata/probable-waffle/tilesets/tiles_2.tsx
+++ apps/client/src/metadata/probable-waffle/tilesets/tiles_2.tsx
@@ -1,18 +1,4 @@
-<?xml;
-version = "1.0";
-encoding = "UTF-8" ?
->
-<
-tileset;
-version = "1.10";
-tiledversion = "1.10.2";
-name = "tiles_2";
-tilewidth = "64";
-tileheight = "32";
-tilecount = "18";
-columns = "18" >
-  < image;
-source = "../../../assets/probable-waffle/tilesets/tiles_2.png";
-width = "1152";
-height = "32" / >
-  < /tileset>;
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles_2" tilewidth="64" tileheight="32" tilecount="18" columns="18">
+ <image source="../../../assets/probable-waffle/tilesets/tiles_2.png" width="1152" height="32"/>
+</tileset>
