6dbfc5110e344cb4ca89f53c2e3e37a0c5a11333
Jernej Habjan <jhabjan@marinexchange.com>
2023-12-24 19:40:26 +0100

    Frost forge wip

--- apps/client/src/app/probable-waffle/game/prefabs/buildings/skaduwee/FrostForge.ts
+++ apps/client/src/app/probable-waffle/game/prefabs/buildings/skaduwee/FrostForge.ts
@@ -2,27 +2,47 @@
 
 /* START OF COMPILED CODE */
 
-import Phaser from "phaser";
+import ActorContainer from "../../../entity/actor/ActorContainer";
 /* START-USER-IMPORTS */
 /* END-USER-IMPORTS */
 
-export default class FrostForge extends Phaser.GameObjects.Container {
+export default class FrostForge extends ActorContainer {
+  constructor(scene: Phaser.Scene, x?: number, y?: number) {
+    super(scene, x ?? 129, y ?? 323.7847134131514);
 
-	constructor(scene: Phaser.Scene, x?: number, y?: number) {
-		super(scene, x ?? 129, y ?? 323.7847134131514);
+    this.removeInteractive();
+    this.setInteractive(
+      new Phaser.Geom.Polygon(
+        "-68.7545786628312 -214.43394506509725 -38.690327299456825 -228.69121890752223 -0.2576760720504012 -233.34032994309558 43.44396766233913 -227.14151522899778 69.16904872584504 -214.74388580080216 71.27223590831247 -192.47146282907468 60.98195252153286 -182.48068661227393 62.22171546435243 -148.38720568473596 69.97023385697469 -142.18839097063815 75.4034707557831 -79.55104366487666 85.32112237919412 -46.381415218517645 96.47898886457017 -37.703074618780704 106.60762571945156 11.039650145841563 74.37378920614296 32.73550164518389 36.251078714441434 42.65360518774037 6.371750760001362 44.99389139705903 -37.82475711902734 42.96354592344528 -76.25740834643375 32.73550164518389 -106.16641807554313 10.997986227988292 -98.7969319792297 -38.23869519017006 -87.09176657806283 -49.2553214500918 -75.38660117689597 -80.92812194736686 -73.32098375316063 -142.20810551818164 -65.05851405821932 -148.40495778938765 -60.82475661048905 -182.48068661227393 -70.74286015304554 -192.08884941912552"
+      ),
+      Phaser.Geom.Polygon.Contains
+    );
 
-		// buildings_skaduwee_frost_forge_frost_forge_png
-		const buildings_skaduwee_frost_forge_frost_forge_png = scene.add.image(-1, -18.21845166634691, "factions", "buildings/skaduwee/frost_forge/frost_forge.png");
-		buildings_skaduwee_frost_forge_frost_forge_png.setOrigin(0.5, 0.7957454759361422);
-		this.add(buildings_skaduwee_frost_forge_frost_forge_png);
+    // buildings_skaduwee_frost_forge_frost_forge_png
+    const buildings_skaduwee_frost_forge_frost_forge_png = scene.add.image(
+      -1,
+      -18.21845166634691,
+      "factions",
+      "buildings/skaduwee/frost_forge/frost_forge.png"
+    );
+    buildings_skaduwee_frost_forge_frost_forge_png.setOrigin(0.5, 0.7957454759361422);
+    this.add(buildings_skaduwee_frost_forge_frost_forge_png);
 
-		// frost_forge_flame
-		const frost_forge_flame = scene.add.sprite(-1, -209.5049784310576, "factions", "buildings/skaduwee/frost_forge/flame/frost_forge_flame_1.png");
-		frost_forge_flame.setOrigin(0.5, 0.5151570341464229);
-		frost_forge_flame.play("frost_forge_flame");
-		this.add(frost_forge_flame);
+    // frost_forge_flame
+    const frost_forge_flame = scene.add.sprite(
+      -1,
+      -209.5049784310576,
+      "factions",
+      "buildings/skaduwee/frost_forge/flame/frost_forge_flame_1.png"
+    );
+    frost_forge_flame.setOrigin(0.5, 0.5151570341464229);
+    frost_forge_flame.play("frost_forge_flame");
+    this.add(frost_forge_flame);
 
-		/* START-USER-CTR-CODE */
+    // this (prefab fields)
+    this.z = 0;
+
+    /* START-USER-CTR-CODE */
 
     this.on("pointerdown", () => {
       buildings_skaduwee_frost_forge_frost_forge_png.setTint(0xff0000); // Tint to red
@@ -32,9 +52,9 @@ export default class FrostForge extends Phaser.GameObjects.Container {
       }, 1000);
     });
     /* END-USER-CTR-CODE */
-	}
+  }
 
-	/* START-USER-CODE */
+  /* START-USER-CODE */
 
   // Write your code here.
 
