00cabfdf5c973c27cf57c989ff072842a0428d26
Jernej Habjan <jhabjan@marinexchange.com>
2024-09-06 09:00:02 +0200

    Minimap fill with actors and minimap click support

--- apps/client/src/app/probable-waffle/game/scenes/HudProbableWaffle.ts
+++ apps/client/src/app/probable-waffle/game/scenes/HudProbableWaffle.ts
@@ -10,14 +10,22 @@ import { HudElementVisibilityHandler } from "../hud/hud-element-visibility.handl
 import { CursorHandler } from "../world/managers/controllers/input/cursor.handler";
 import { MultiSelectionHandler } from "../world/managers/controllers/input/multi-selection.handler";
 import { Subscription } from "rxjs";
-import { GameSessionState } from "@fuzzy-waddle/api-interfaces";
+import { GameSessionState, Vector2Simple } from "@fuzzy-waddle/api-interfaces";
 import { SaveGame } from "../data/save-game";
 import { onScenePostCreate } from "../data/game-object-helper";
 import { getSceneComponent } from "./components/scene-component-helpers";
 import { TilemapComponent } from "./components/tilemap.component";
+import { getActorComponent } from "../data/actor-component";
+import { ColliderComponent } from "../entity/actor/components/collider-component";
+import { getTileCoordsUnderObject } from "../library/tile-under-object";
 /* END-USER-IMPORTS */
 
 export default class HudProbableWaffle extends ProbableWaffleScene {
+  private minimapDiamonds: Phaser.GameObjects.Polygon[] = [];
+  private actorDiamonds: Phaser.GameObjects.Polygon[] = [];
+  private readonly minimapWidth = 400;
+  private readonly smallMinimapWidth = 200;
+  private readonly minimapBreakpoint = 800;
   constructor() {
     super("HudProbableWaffle");
 
@@ -92,63 +100,140 @@ export default class HudProbableWaffle extends ProbableWaffleScene {
   }
 
   private postSceneCreate() {
-    if (!this.parentScene) return;
-    const tileMapComponent = getSceneComponent(this.parentScene, TilemapComponent);
-    if (tileMapComponent) {
-      const width = tileMapComponent.tilemap.width;
-      const widthInPixels = 400;
-      const y = this.scale.height - widthInPixels / 2;
-      const data = tileMapComponent.data;
-      this.createIsometricMinimap(widthInPixels, width, data, 0, y);
-    }
+    this.redrawMinimap();
   }
 
-  /**
-   * create isometric minimap.
-   * draw a diamond shape of the minimap.
-   * Tiles are for now of green color
-   * objects are of red color
-   */
+  private redrawMinimap() {
+    if (!this.parentScene) return;
+    const tileMapComponent = getSceneComponent(this.parentScene, TilemapComponent);
+    if (!tileMapComponent) throw new Error("Tilemap component not found on parent scene");
+    const width = tileMapComponent.tilemap.width;
+    const sceneWidth = this.scale.width;
+    const widthInPixels = sceneWidth > this.minimapBreakpoint ? this.minimapWidth : this.smallMinimapWidth;
+    const y = this.scale.height - widthInPixels / 2;
+    const data = tileMapComponent.data;
+    this.createIsometricMinimap(widthInPixels, width, 0, y, data);
+    this.fillMinimapWithActors(widthInPixels, width, 0, y); // todo? subscribe to actor added/removed/moved
+  }
+
+  private createDiamondShape(
+    x: number,
+    y: number,
+    isoX: number,
+    isoY: number,
+    pixelWidth: number,
+    pixelHeight: number,
+    color: Phaser.Display.Color
+  ): Phaser.GameObjects.Polygon {
+    const diamondPoints = [
+      { x: isoX, y: isoY + pixelHeight / 2 },
+      { x: isoX + pixelWidth / 2, y: isoY },
+      { x: isoX + pixelWidth, y: isoY + pixelHeight / 2 },
+      { x: isoX + pixelWidth / 2, y: isoY + pixelHeight }
+    ];
+    const diamond = this.add.polygon(x + pixelWidth / 2, y + pixelHeight / 2, diamondPoints, color.color);
+    diamond.setInteractive(new Phaser.Geom.Polygon(diamondPoints), Phaser.Geom.Polygon.Contains);
+    diamond.depth = this.isometricMinimapDepth;
+    return diamond;
+  }
+
   private createIsometricMinimap(
     widthInPixels: number,
     widthInTiles: number,
-    layerData: Phaser.Tilemaps.Tile[][],
     x: number,
-    y: number
+    y: number,
+    layerData: Phaser.Tilemaps.Tile[][]
   ) {
     const height = widthInPixels / 2;
     const pixelWidth = widthInPixels / widthInTiles;
     const pixelHeight = height / widthInTiles;
-
-    // Calculate the center of the minimap
     const centerX = widthInPixels / 2;
-
-    // Offset to start drawing from the center
     const offsetX = centerX - pixelWidth / 2;
 
-    // fill first 5 tiles with red color
+    this.minimapDiamonds.forEach((diamond) => diamond.destroy());
+    this.minimapDiamonds = [];
     for (let i = 0; i < widthInTiles; i++) {
       for (let j = 0; j < widthInTiles; j++) {
         const tile = layerData[i][j];
         const color = this.getColorFromTiledProperty(tile) ?? Phaser.Display.Color.RandomRGB();
         const isoX = offsetX + (j - i) * (pixelWidth / 2);
         const isoY = (i + j) * (pixelHeight / 2);
-        // create diamond shape
-        const diamondPoints = [
-          { x: isoX, y: isoY + pixelHeight / 2 },
-          { x: isoX + pixelWidth / 2, y: isoY },
-          { x: isoX + pixelWidth, y: isoY + pixelHeight / 2 },
-          { x: isoX + pixelWidth / 2, y: isoY + pixelHeight }
-        ];
-        // create polygon, add it to minimap and make it interactive
-        const diamond = this.add.polygon(x + pixelWidth / 2, y + pixelHeight / 2, diamondPoints, color.color);
-        diamond.setInteractive(new Phaser.Geom.Polygon(diamondPoints), Phaser.Geom.Polygon.Contains);
-        diamond.on("pointerdown", () => {
-          console.log("clicked on minimap on tile", i, j);
+        const diamond = this.createDiamondShape(x, y, isoX, isoY, pixelWidth, pixelHeight, color);
+        diamond.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+          if (pointer.rightButtonDown()) {
+            this.assignActorActionToTileCoordinates({ x: i, y: j });
+            // Handle right-click logic here
+          } else if (pointer.leftButtonDown()) {
+            this.moveCameraToTileCoordinates({ x: i, y: j });
+          }
         });
-        diamond.depth = this.isometricMinimapDepth;
+        this.minimapDiamonds.push(diamond);
       }
     }
+  }
+
+  private getIsometricCoordinates(tileXY: Vector2Simple): { isoX: number; isoY: number } | null {
+    const { x, y } = tileXY;
+
+    if (!this.parentScene) throw new Error("Parent scene not set");
+    const tileMapComponent = getSceneComponent(this.parentScene, TilemapComponent);
+    if (!tileMapComponent) throw new Error("Tilemap component not found on parent scene");
+
+    const tile = tileMapComponent.tilemap.getTileAt(x, y);
+    if (!tile) return null;
+
+    const isoX = (y - x) * (tile.width / 2);
+    const isoY = (x + y) * (tile.height / 2);
+
+    return { isoX, isoY };
+  }
+
+  private moveCameraToTileCoordinates(tileXY: Vector2Simple) {
+    if (!this.parentScene) throw new Error("Parent scene not set");
+    const isoCoords = this.getIsometricCoordinates(tileXY);
+    if (!isoCoords) return;
+
+    const { isoX, isoY } = isoCoords;
+    this.parentScene.cameras.main.pan(isoX, isoY, 50, Phaser.Math.Easing.Quadratic.InOut);
+  }
+
+  private assignActorActionToTileCoordinates(tileXY: Vector2Simple) {
+    if (!this.parentScene) throw new Error("Parent scene not set");
+    const isoCoords = this.getIsometricCoordinates(tileXY);
+    if (!isoCoords) return;
+
+    const { isoX, isoY } = isoCoords;
+    console.log("Assigning action to tile coordinates", isoX, isoY);
+    this.parentScene.events.emit("assignActorActionToTileCoordinates", { x: isoX, y: isoY }); // todo
+  }
+
+  private fillMinimapWithActors(widthInPixels: number, widthInTiles: number, x: number, y: number) {
+    if (!this.parentScene) throw new Error("Parent scene not set");
+    const tileMapComponent = getSceneComponent(this.parentScene, TilemapComponent);
+    if (!tileMapComponent) throw new Error("Tilemap component not found on parent scene");
+
+    const height = widthInPixels / 2;
+    const pixelWidth = widthInPixels / widthInTiles;
+    const pixelHeight = height / widthInTiles;
+    const centerX = widthInPixels / 2;
+    const offsetX = centerX - pixelWidth / 2;
+
+    this.actorDiamonds.forEach((diamond) => diamond.destroy());
+    this.actorDiamonds = [];
+    this.parentScene.scene.scene.children.each((child) => {
+      const colliderComponent = getActorComponent(child, ColliderComponent);
+      if (!colliderComponent) return;
+
+      const tilesUnderObject: Vector2Simple[] = getTileCoordsUnderObject(tileMapComponent.tilemap, child);
+      const color = Phaser.Display.Color.RandomRGB();
+
+      for (const tile of tilesUnderObject) {
+        const isoX = offsetX + (tile.x - tile.y) * (pixelWidth / 2);
+        const isoY = (tile.x + tile.y) * (pixelHeight / 2);
+        const diamond = this.createDiamondShape(x, y, isoX, isoY, pixelWidth, pixelHeight, color);
+        this.actorDiamonds.push(diamond);
+      }
+    });
   }
 
   private getColorFromTiledProperty(tile: Phaser.Tilemaps.Tile): Phaser.Display.Color | null {
@@ -173,6 +258,9 @@ export default class HudProbableWaffle extends ProbableWaffleScene {
     // set save button to the left of quit button by 20px
     this.buttonSave.x = this.buttonQuit.x - this.buttonSave.w - 40;
     this.buttonSave.y = this.buttonQuit.y;
+
+    // redraw minimap
+    this.redrawMinimap();
   }
 
   private handleQuit() {
