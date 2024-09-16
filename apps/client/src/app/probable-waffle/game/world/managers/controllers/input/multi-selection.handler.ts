3edbfb8cb197c04ff046456e4cbd75dbbdd9f12b
Jernej Habjan <jhabjan@marinexchange.com>
2024-09-16 19:13:27 +0200

    Pointer events on mobile to move camera

--- apps/client/src/app/probable-waffle/game/world/managers/controllers/input/multi-selection.handler.ts
+++ apps/client/src/app/probable-waffle/game/world/managers/controllers/input/multi-selection.handler.ts
@@ -43,6 +43,8 @@ export class MultiSelectionHandler {
   }
 
   private setupEvents() {
+    const touchEnabled = this.hudScene.input.manager.touch;
+    if (touchEnabled) return;
     this.hudScene.input.on(Input.Events.POINTER_DOWN, this.handlePointerDown, this);
     this.hudScene.input.on(Input.Events.POINTER_MOVE, this.handlePointerMove, this);
     this.hudScene.input.on(Input.Events.POINTER_UP, this.handlePointerUp, this);
