0cf1eac0d35b2322f9c955737328b924d7ec8c26
Jernej Habjan <jhabjan@marinexchange.com>
2025-02-03 07:33:26 +0100

    #273 Fix tests

--- apps/client/src/app/home/music/music.component.spec.ts
+++ apps/client/src/app/home/music/music.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { MusicComponent } from "./music.component";
+import { ProfileNavComponent } from "../profile/profile-nav/profile-nav.component";
+import { HomeNavComponent } from "../../shared/components/home-nav/home-nav.component";
+import { HomeNavTestingComponent } from "../../shared/components/home-nav/home-nav.component.spec";
 
 describe("MusicComponent", () => {
   let component: MusicComponent;
@@ -8,7 +11,16 @@ describe("MusicComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MusicComponent]
-    }).compileComponents();
+    })
+      .overrideComponent(MusicComponent, {
+        remove: {
+          imports: [HomeNavComponent]
+        },
+        add: {
+          imports: [HomeNavTestingComponent]
+        }
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(MusicComponent);
     component = fixture.componentInstance;
