ceb9e54b229c0b3d3d78cd4ea6d8b42347082a64
Jernej Habjan <jhabjan@marinexchange.com>
2025-04-06 10:04:22 +0200

    #323 Splitting audio files

--- apps/client/src/metadata/sfx/generate-audio-sprites.js
+++ apps/client/src/metadata/sfx/generate-audio-sprites.js
@@ -18,9 +18,61 @@ const audioConfigs = {
     pattern: "metadata/probable-waffle/sfx/environment/**/*.mp3",
     outputPath: "assets/probable-waffle/sfx/environment"
   },
-  probableWaffleActors: {
-    pattern: "metadata/probable-waffle/sfx/actors/**/*.mp3",
-    outputPath: "assets/probable-waffle/sfx/actors"
+  probableWaffleActorsFoliage: {
+    pattern: "metadata/probable-waffle/sfx/actors/foliage/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/foliage"
+  },
+  probableWaffleActorsAnimals: {
+    pattern: "metadata/probable-waffle/sfx/actors/animals/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/animals"
+  },
+  probableWaffleActorsResources: {
+    pattern: "metadata/probable-waffle/sfx/actors/resources/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/resources"
+  },
+  probableWaffleActorsTivaraMaceman: {
+    pattern: "metadata/probable-waffle/sfx/actors/Tivara/Maceman/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Tivara/Maceman"
+  },
+  probableWaffleActorsTivaraOlival: {
+    pattern: "metadata/probable-waffle/sfx/actors/Tivara/Olival/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Tivara/Olival"
+  },
+  probableWaffleActorsTivaraSlingshot: {
+    pattern: "metadata/probable-waffle/sfx/actors/Tivara/Slingshot/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Tivara/Slingshot"
+  },
+  probableWaffleActorsTivaraWorkerFemale: {
+    pattern: "metadata/probable-waffle/sfx/actors/Tivara/Worker Female/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Tivara/Worker Female"
+  },
+  probableWaffleActorsTivaraWorkerMale: {
+    pattern: "metadata/probable-waffle/sfx/actors/Tivara/Worker Male/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Tivara/Worker Male"
+  },
+  probableWaffleActorsSkaduweeMagician: {
+    pattern: "metadata/probable-waffle/sfx/actors/Skaduwee/Magician/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Skaduwee/Magician"
+  },
+  probableWaffleActorsSkaduweeOwl: {
+    pattern: "metadata/probable-waffle/sfx/actors/Skaduwee/Owl/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Skaduwee/Owl"
+  },
+  probableWaffleActorsSkaduweeRanged: {
+    pattern: "metadata/probable-waffle/sfx/actors/Skaduwee/Ranged/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Skaduwee/Ranged"
+  },
+  probableWaffleActorsSkaduweeWarrior: {
+    pattern: "metadata/probable-waffle/sfx/actors/Skaduwee/Warrior/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Skaduwee/Warrior"
+  },
+  probableWaffleActorsSkaduweeWorkerFemale: {
+    pattern: "metadata/probable-waffle/sfx/actors/Skaduwee/Worker Female/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Skaduwee/Worker Female"
+  },
+  probableWaffleActorsSkaduweeWorkerMale: {
+    pattern: "metadata/probable-waffle/sfx/actors/Skaduwee/Worker Male/**/*.mp3",
+    outputPath: "assets/probable-waffle/sfx/actors/Skaduwee/Worker Male"
   }
 };
 
