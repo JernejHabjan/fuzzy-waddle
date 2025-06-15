cf323b96340975374e6fcb50a127bb031f58849f
Jernej Habjan <jhabjan@marinexchange.com>
2025-06-15 16:34:07 +0200

    #126 Fix usage of primary colors

--- apps/client/src/app/shared/loader/loader.component.ts
+++ apps/client/src/app/shared/loader/loader.component.ts
@@ -3,26 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "fuzzy-waddle-loader",
   template: `<div class="loader"></div>`,
-  styles: [
-    `
-      .loader {
-        border: 16px solid #ffe6c2;
-        border-top: 16px solid #9f3000;
-        border-radius: 50%;
-        width: 120px;
-        height: 120px;
-        animation: spin 2s linear infinite;
-      }
-
-      @keyframes spin {
-        0% {
-          transform: rotate(0deg);
-        }
-        100% {
-          transform: rotate(360deg);
-        }
-      }
-    `
-  ]
+  styleUrls: ["./loader.component.scss"],
 })
 export class LoaderComponent {}
