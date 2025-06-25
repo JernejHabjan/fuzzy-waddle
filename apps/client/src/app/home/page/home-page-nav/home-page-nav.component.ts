2bd3fe41507e77e17a659f69d4141297851aa6cf
Jernej Habjan <jhabjan@marinexchange.com>
2025-06-25 18:39:14 +0200

    #126 Home page nav personalized

--- apps/client/src/app/home/page/home-page-nav/home-page-nav.component.ts
+++ apps/client/src/app/home/page/home-page-nav/home-page-nav.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from "@angular/core";
 import { AuthService } from "../../../auth/auth.service";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { HomeNavComponent } from "../../../shared/components/home-nav/home-nav.component";
 import { RouterLink } from "@angular/router";
+import { AvatarProviderService } from "../../../shared/components/chat/avatar-provider/avatar-provider.service";
 
 @Component({
   selector: "probable-waffle-home-page-nav",
@@ -13,8 +13,8 @@ import { RouterLink } from "@angular/router";
   imports: [FaIconComponent, HomeNavComponent, RouterLink]
 })
 export class HomePageNavComponent {
-  protected readonly faUser = faUser;
   protected readonly faGoogle = faGoogle;
 
   protected readonly authService = inject(AuthService);
+  protected readonly avatarProviderService = inject(AvatarProviderService);
 }
