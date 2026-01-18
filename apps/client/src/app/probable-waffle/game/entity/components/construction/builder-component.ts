971a83e0d6a073667e87839e54102146e44aa75e
Jernej Habjan <jhabjan@marinexchange.com>
2026-01-18 16:38:26 +0100

    #368 Builder - increase construction seek range

--- apps/client/src/app/probable-waffle/game/entity/components/construction/builder-component.ts
+++ apps/client/src/app/probable-waffle/game/entity/components/construction/builder-component.ts
@@ -157,7 +157,7 @@ export class BuilderComponent {
    * will seek for new construction sites in this range
    */
   getConstructionSeekRange(): number {
-    return 10;
+    return 30;
   }
 
   getRepairRange() {
