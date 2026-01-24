e2807bb209920aa1c3cafa9aae06bc69d7d78bbd
Jernej Habjan <jernej.habjan96@gmail.com>
2026-01-24 12:59:09 +0100

    #237 Chat - view previous messages (#545)
    * #237 Chat - previous messages plan
    
    * #237 Chat implementation
    
    * #237 Update gitignore
    
    * Chat fixes
    
    * Fixup tests
    
    * #237 Cleanup
    
    * #237 FlySquasher constraint rename
    
    * #237 Messages constraint to auth.users
    
    * #237 FK to profiles fixes
    
    * #237 Cr cleanup
    
    ---------
    
    Co-authored-by: Jernej Habjan <jhabjan@marinexchange.com>

--- apps/client/src/app/data-access/chat/chat.service.interface.ts
+++ apps/client/src/app/data-access/chat/chat.service.interface.ts
@@ -1,7 +1,8 @@
-import { type ChatMessage } from "@fuzzy-waddle/api-interfaces";
+import { type ChatMessage, type GetMessagesResponseDto } from "@fuzzy-waddle/api-interfaces";
 import { Observable } from "rxjs";
 
 export interface IChatService {
   sendMessage(msg: ChatMessage): Promise<void>;
   getMessageListener(): Promise<Observable<ChatMessage> | undefined>;
+  getMessages(limit?: number, offset?: number, gameInstanceId?: string): Promise<GetMessagesResponseDto>;
 }
