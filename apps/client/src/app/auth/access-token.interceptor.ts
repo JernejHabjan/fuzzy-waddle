54023e06fb7ee5cfd9df74a7fa7ec4c54056f5cd
Jernej Habjan <jhabjan@marinexchange.com>
2026-06-06 20:56:10 +0200

    separate auth and access token interceptors

--- apps/client/src/app/auth/access-token.interceptor.ts
+++ apps/client/src/app/auth/access-token.interceptor.ts
@@ -1,34 +1,26 @@
-import { inject, Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
-import { type HttpEvent, HttpHandler, HttpHeaders, type HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { from, Observable } from "rxjs";
-import { switchMap } from "rxjs/operators";
+import type { HttpInterceptorFn } from "@angular/common/http";
 import { environment } from "../../environments/environment";
+import { inject } from "@angular/core";
 
-@Injectable({
-  providedIn: "root"
-})
-export class AccessTokenInterceptor implements HttpInterceptor {
-  private readonly authService = inject(AuthService);
+export const accessTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (!request.url.startsWith(environment.api)) {
-      return next.handle(request);
-    }
-
-    return from(this.authService.ensureAuthReady()).pipe(
-      switchMap(() => {
-        let authenticatedRequest = request;
-        const accessToken = this.authService.accessToken;
-        if (accessToken) {
-          const headers = new HttpHeaders({
-            Authorization: `Bearer ${accessToken}`
-          });
-          authenticatedRequest = request.clone({ headers });
-        }
-
-        return next.handle(authenticatedRequest);
-      })
-    );
+  if (!req.url.startsWith(environment.api)) {
+    return next(req);
   }
-}
+
+  const accessToken = authService.accessToken;
+
+  if (!accessToken) {
+    return next(req);
+  }
+
+  return next(
+    req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+  );
+};
