dad027679fd0e86b1646b8fa1260efc274a5b781
Jernej Habjan <jhabjan@marinexchange.com>
2026-06-30 20:02:52 +0200

    646 Fly squasher color

--- apps/client/src/app/fly-squasher/game/fly/components/fly-representable-component.ts
+++ apps/client/src/app/fly-squasher/game/fly/components/fly-representable-component.ts
@@ -98,8 +98,8 @@ export class FlyRepresentableComponent implements IFlyBase {
     const millisecondsSinceKilled = new Date().getTime() - killedAt.getTime();
 
     const hexColor = Phaser.Display.Color.Interpolate.ColorWithColor(
-      Phaser.Display.Color.HexStringToColor("#ffffff"),
-      Phaser.Display.Color.HexStringToColor("#ff0000"),
+      Phaser.Display.Color.HexStringToColor("#fd6d6d"),
+      Phaser.Display.Color.HexStringToColor("#271f15"),
       despawnTimeInMilliseconds,
       millisecondsSinceKilled
     );
